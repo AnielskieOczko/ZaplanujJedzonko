070f073863ace3810205606f5fc722f7555de2e1
Rafał Jankowski <rafaljankowski7@gmail.com>
2024-04-16 21:36:41 +0200

    Update AdminDao.java

--- src/main/java/pl/coderslab/dao/AdminDao.java
+++ src/main/java/pl/coderslab/dao/AdminDao.java
@@ -1,5 +1,34 @@
 package pl.coderslab.dao;
 
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdminDao {
-    
+
+    public List<Admin> findAll() {
+        List<Admin> admins = new ArrayList<>();
+        return admins;
+    }
+
+    public Admin getById(Integer id) {
+        return new Admin();
+    }
+
+    public Admin create(Admin admin) {
+        return new Admin();
+    }
+
+    public void delete(Integer id) {
+
+    }
+
+    public void update(Admin admin) {
+
+    }
+
+    public String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
 }
