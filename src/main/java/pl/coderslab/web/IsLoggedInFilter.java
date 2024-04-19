34a70a909ade7835ef3c31e60611b9c104ba5c49
mateuszmarc <mmarckiewicz@gmail.com>
2024-04-19 16:18:53 +0000

    Create IsLoggedInFilter - Create doFilter method checking if Admin is authenticated

---
+++ src/main/java/pl/coderslab/web/IsLoggedInFilter.java
@@ -0,0 +1,47 @@
+package pl.coderslab.web;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("/app/*")
+public class IsLoggedInFilter implements Filter {
+    public static final Logger logger = LogManager.getLogger(IsLoggedInFilter.class);
+
+
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpSession session = request.getSession();
+        if (session.getAttribute("adminId") != null) {
+            int adminId = Integer.parseInt((String) session.getAttribute("adminId"));
+            if (adminId != 0) {
+                logger.info("Admin is logged in");
+                filterChain.doFilter(request, response);
+            }
+        } else {
+            logger.info("Admin is not logged in");
+            response.sendRedirect("/login");
+        }
+
+
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
