85fb691c3e6489f78f6dfcc4b5760c8004c90c3e
mateuszmarc <mmarckiewicz@gmail.com>
2024-04-20 00:58:04 +0000

    Remove casting of adminId parameter into int data type. Remove conditional statement checking if adminId parameter != 0

--- src/main/java/pl/coderslab/web/IsLoggedInFilter.java
+++ src/main/java/pl/coderslab/web/IsLoggedInFilter.java
@@ -13,18 +13,15 @@ import java.io.IOException;
 @WebFilter("/app/*")
 public class IsLoggedInFilter implements Filter {
     public static final Logger logger = LogManager.getLogger(IsLoggedInFilter.class);
-    
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         HttpSession session = request.getSession();
         if (session.getAttribute("adminId") != null) {
-            int adminId = Integer.parseInt((String) session.getAttribute("adminId"));
-            if (adminId != 0) {
-                logger.info("Admin is logged in");
-                filterChain.doFilter(request, response);
-            }
+            logger.info("Admin is logged in");
+            filterChain.doFilter(request, response);
         } else {
             logger.info("Admin is not logged in");
             response.sendRedirect("/login");
