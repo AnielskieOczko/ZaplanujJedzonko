d0b3967c5de9285d8bef07e7739a0ade7e8f71b3
Rafał Jankowski <rafaljankowski7@gmail.com>
2024-04-21 14:34:58 +0200

    Update AddNewRecipe.java
    code cleanup
    added DOTO

--- src/main/java/pl/coderslab/web/AddNewRecipe.java
+++ src/main/java/pl/coderslab/web/AddNewRecipe.java
@@ -3,7 +3,6 @@ package pl.coderslab.web;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import pl.coderslab.dao.AdminDao;
-import pl.coderslab.dao.PlanDao;
 import pl.coderslab.dao.RecipeDao;
 import pl.coderslab.model.Recipe;
 
@@ -18,12 +17,12 @@ import java.io.IOException;
 @WebServlet("/add")
 public class AddNewRecipe extends HttpServlet {
     RecipeDao recipeDao = new RecipeDao();
-    PlanDao planDao = new PlanDao();
     AdminDao adminDao = new AdminDao();
     public static final Logger logger = LogManager.getLogger(IsLoggedInFilter.class);
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        // TODO: remove adminId setAtt
         HttpSession session = req.getSession();
         session.setAttribute("adminId", 1);
         int adminId = (int) session.getAttribute("adminId");
@@ -48,8 +47,9 @@ public class AddNewRecipe extends HttpServlet {
             Recipe recipe = new Recipe(name, ingredients, desc, prepTime, prepDesc, adminId);
             Recipe newRecipe = recipeDao.create(recipe);
             System.out.println(newRecipe);
+            // TODO: change redirect to /app/recipe
             resp.sendRedirect("/dashboard");
-        } catch (RuntimeException e) {
+        } catch (Exception e) {
             logger.error(e.getMessage());
         }
     }
