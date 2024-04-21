22a80b4319b97fc5ec270f4230ab752b60ce7a7a
Rafał Jankowski <rafaljankowski7@gmail.com>
2024-04-21 14:29:34 +0200

    Update AddNewRecipe.java
    added doPost method to servlet addRecipe

--- src/main/java/pl/coderslab/web/AddNewRecipe.java
+++ src/main/java/pl/coderslab/web/AddNewRecipe.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 import pl.coderslab.dao.AdminDao;
 import pl.coderslab.dao.PlanDao;
 import pl.coderslab.dao.RecipeDao;
+import pl.coderslab.model.Recipe;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet("/recipe/add")
+@WebServlet("/add")
 public class AddNewRecipe extends HttpServlet {
     RecipeDao recipeDao = new RecipeDao();
     PlanDao planDao = new PlanDao();
@@ -28,7 +29,28 @@ public class AddNewRecipe extends HttpServlet {
         int adminId = (int) session.getAttribute("adminId");
 
         req.setAttribute("adminName", adminDao.read(adminId).getFirstName());
-
         getServletContext().getRequestDispatcher("/addNewRecipe.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        HttpSession session =  req.getSession();
+        int adminId = (int) session.getAttribute("adminId");
+
+        String name = req.getParameter("recipe-name");
+        String desc = req.getParameter("recipe-desc");
+        int prepTime = Integer.parseInt(req.getParameter("recipe-prep-time"));
+        String prepDesc = req.getParameter("recipe-prep-desc");
+        String ingredients = req.getParameter("recipe-ingredients");
+
+        try {
+            Recipe recipe = new Recipe(name, ingredients, desc, prepTime, prepDesc, adminId);
+            Recipe newRecipe = recipeDao.create(recipe);
+            System.out.println(newRecipe);
+            resp.sendRedirect("/dashboard");
+        } catch (RuntimeException e) {
+            logger.error(e.getMessage());
+        }
     }
 }
