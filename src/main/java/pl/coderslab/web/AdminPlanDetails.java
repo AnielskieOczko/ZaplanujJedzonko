2ca0908609ba3b9a8a87124c4f5ca92c05aa3ea2
mateuszmarc <mmarckiewicz@gmail.com>
2024-04-23 16:33:08 +0000

    Create AdminPlanDetails servlet class. Change PlanDao class:  -Create getPlanForAdmin.  -Refactor FIND_LAST_PLAN_QUERY  -Change query name to retrieve last added plan details to be more generic FIND_PLAN_DETAILS_QUERY- this query is resused in getPlanFroAdmin method

---
+++ src/main/java/pl/coderslab/web/AdminPlanDetails.java
@@ -0,0 +1,33 @@
+package pl.coderslab.web;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pl.coderslab.dao.PlanDao;
+import pl.coderslab.dto.PlanDto;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+
+
+@WebServlet("/app/plan/details")
+public class AdminPlanDetails extends HttpServlet {
+    public static final Logger logger = LogManager.getLogger(AdminPlanDetails.class);
+    public static final PlanDao planDao = new PlanDao();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+
+        int planId = Integer.parseInt(req.getParameter("id"));
+
+        PlanDto planDto = planDao.getPlanForAdmin(planId);
+        logger.info(planDto);
+
+
+    }
+}
